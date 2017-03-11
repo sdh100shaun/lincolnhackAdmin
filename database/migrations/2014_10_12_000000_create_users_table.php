c398eacc195b2011bf30fed388fb9ec655b4bc55
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-03-11 21:10:11 +0000

    initial setup

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Jenssegers\Mongodb\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function($collection)
+        {
+            $collection->index('name');
+            $collection->unique('email');
+            $collection->string('password');
+            $collection->timestamps();
+            $collection->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       
+    }
+}
