e99fcf56bd77e6439c7f029616a1e37c3a0c853b
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-03-11 22:58:59 +0000

    changed user class

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => Jenssegers\Mongodb\Auth\User::class,
         ],
 
         // 'users' => [
