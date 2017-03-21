62ddfef9bafbbc1c97ebef1c294820f6fe3f8350
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-03-21 20:51:32 +0000

    intro para

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,52 @@
 
 @include('partials.header')
 
-<section>
+<section class="light">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <h2 class="sectionHeader"> What is Lincolnhack ? </h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-5 col-md-offset-1">
 
 
+            <img src="http://placehold.it/350x350" class="img-responsive"/>
 
+        </div>
+
+
+        <div class="col-md-5">
+            <p>
+                Lincoln Hack is a free 24 hour coding competition where teams of up to four people compete for fantastic
+                prizes. They start with an idea and build it in to something. Hardware hacks, software hacks, API
+                mash-ups, funny things, arty things, noisy things... whatever you decide to create, you’ve got 24 hours
+                to do it!
+
+            </p>
+            <p>
+                Lincoln Hack is in it's second year and is still celebrating it's success from last year.
+                This year, it is being held to coincide with Lincolnshire Tech Week, and moves to a new
+                building.
+                We plan to keep the great atmosphere though and fun and mix of great people, from students to veterans
+                all are welcome as long as your interested in tech, willing to build something and enjoy meeting like minded
+                people you will enjoy it we are sure.
+            </p>
+        </div>
+    </div>
+</section>
+<section class="light flex-center position-ref">
+
+
+    <div class="row">
+        <div class="col-md-5 col-md-offset-1">
+
+
+        </div>
+
+
+        <div class="col-md-5">
+
+        </div>
+    </div>
 </section>
