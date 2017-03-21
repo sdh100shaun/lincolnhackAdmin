bb79815deaca35424ef51756d22e545516b4216b
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-03-21 19:39:20 +0000

    creating partials to keep the views clean

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+<header class="container-fluid">
+    @if (Route::has('login'))
+        <div class="top-right links pull-right">
+            @if (Auth::check())
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ url('/login') }}">Admin</a>
+            @endif
+        </div>
+    @endif
+
+
+    <div class="row  col-md-12 container-fluid">
+        <div class="jumbotron no-background flex-center position-ref full-height">
+
+
+            <img src="../../images/lincoln-hack-imp.png" alt="lincolnhack"/>
+
+        </div>
+        <div class="flex-center position-ref full-height">
+            <h1>Lincoln<strong>Hack</strong></h1>
+        </div>
+
+        <h2>24 Hours Of Amazing Coding Fun! in the capital of Lincolnshire</h2>
+        <h3>20th and 21st May 2017</h3>
+    </div>
+
+</header>
