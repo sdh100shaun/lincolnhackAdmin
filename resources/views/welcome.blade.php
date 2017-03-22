ee15aebb3ed7fa65b377aa7d6f94d9c7cbd938b3
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-03-22 18:26:21 +0000

    updated copy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,6 +42,7 @@
             </p>
 
             <p>
+                <span class="highlight">And it is a FREE event. </span>
                 There is no excuse not to come along ....
                 <a href="http://eventbrite.com" class="btn btn-default tickets center">GET TICKETS NOW <i class="ticket-link"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512.001 512.001" style="enable-background:new 0 0 512.001 512.001;" xml:space="preserve" width="512px" height="512px">
 <g>
