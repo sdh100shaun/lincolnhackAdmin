a2e7204ef414f843bd09720d22d60c27b7c6a107
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-03-26 22:21:31 +0100

    updating design

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>LincolnHack</title>
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/app.css" />
     <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
     <!-- Latest compiled and minified JavaScript -->
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -17,257 +17,6 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-    <style>
-        body {
-            font-size: 16px;
-        }
-
-        .root {
-            background-image: url("images/header-bg-2000x1151.jpg");
-            background-size: cover;
-            width: 100%;
-            height: 900px;
-            position: relative;
-            color: #fff;
-        }
-
-        .subtitle {
-            font-size: 27px;
-            font-style: italic;
-        }
-
-        .maintitle {
-            display: block;
-            color: #fff;
-            text-transform: uppercase;
-            letter-spacing: 1px;
-            font-size: 260%;
-            border: 5px solid;
-            margin: 20px auto;
-            clear: both;
-            max-width: 320px;
-            font-weight: 100;
-        }
-
-        .h--1,
-        .h--2 {
-            font-size: 36px;
-        }
-
-        .h--3 {
-            font-size: 28px;
-        }
-
-        .h--4,
-        .h--5 {
-            font-size: 20px;
-        }
-
-        .h--1 {
-            margin-top: 0;
-            margin-bottom: 30px;
-            text-transform: uppercase;
-        }
-
-        .h--4 {
-            text-align: center;
-            margin: 20px 0;
-        }
-
-        .xs-center {
-            text-align: center;
-        }
-
-        .btn {
-            background: none;
-        }
-
-        .btn-primary {
-            background-color: #5e83dc;
-            border-color: #5e83dc;
-            color: #ffffff;
-        }
-
-        .wrapper {
-            padding-top: 50px;
-            padding-bottom: 50px;
-        }
-
-        .wrapper--grey {
-            background-color: rgb(46, 46, 46);
-            color: #fff;
-        }
-
-        .img-promo {
-            margin: 40px auto 40px auto;
-            width: 500px;
-            border: 1px solid #fff;
-        }
-        .img-promo:nth-of-type(2) {
-            margin: 0px auto 40px auto;
-        }
-
-        .arrow {
-            background: rgba(0, 0, 0, 0.2);
-            border-radius: 3px;
-            color: #fff;
-            display: inline-block;
-            height: 46px;
-            outline-style: none;
-            position: relative;
-            text-decoration: none;
-            transition: all .3s ease-in-out;
-            width: 46px;
-        }
-
-        .arrow a {
-            color: #fff;
-            display: block;
-            line-height: 46px;
-        }
-
-        .parallax {
-            background-image: url(images/header-bg-2000x1151.jpg);
-            min-height: 100px;
-            background-attachment: fixed;
-            background-position: center;
-            background-repeat: no-repeat;
-            background-size: cover;
-            color: #fff;
-            padding: 35px 0;
-        }
-
-        .parallax--boole {
-            background-image: url(images/boole-overview-bg-2000x1731.jpg);
-            color: #000;
-        }
-
-        .parallax--group {
-            background-image: url(images/footer-bg-2000x1334.jpg);
-            color: #000;
-            padding: 170px;
-        }
-
-        .panel-title>.small, .panel-title>.small>a, .panel-title>a, .panel-title>small, .panel-title>small>a {
-    color: inherit;
-    display: block;
-    padding: 10px 20px;
-    margin: -10px;
-}
-
-.panel-default>.panel-heading+.panel-collapse>.panel-body {
-    color: #000;
-}
-        /* ----------------------------------------------------------------------------
-       Sponsors
-   ---------------------------------------------------------------------------- */
-
-        .sponsors {
-            font-size: 0;
-            line-height: 0;
-            text-align: center;
-        }
-
-        .sponsor {
-            margin: 15px;
-            display: inline-block;
-            background: #fff;
-        }
-
-        .sponsor:not(.inline) {
-            position: relative;
-            vertical-align: top;
-            border: 2px solid #e9e9e9;
-        }
-
-        .sponsor:not(.inline) img {
-            -webkit-transform: translateX(-50%) translateY(-50%);
-            -moz-transform: translateX(-50%) translateY(-50%);
-            -o-transform: translateX(-50%) translateY(-50%);
-            -ms-transform: translateX(-50%) translateY(-50%);
-            transform: translateX(-50%) translateY(-50%);
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            display: block;
-            max-width: 80%;
-            max-height: 80%;
-        }
-
-        .sponsor:not(.inline):not(.big) {
-            width: 160px;
-            height: 160px;
-        }
-
-        .sponsor.big {
-            width: 255px;
-            height: 255px;
-        }
-
-        .sponsor.inline {
-            vertical-align: middle;
-        }
-
-        @media (max-width: 1199px) {
-            .sponsor:not(.inline):not(.big) {
-                margin: 8px;
-                width: 140px;
-                height: 140px;
-            }
-            .sponsor.big {
-                margin: 15px;
-                width: 205px;
-                height: 205px;
-            }
-        }
-
-        @media (max-width: 991px) {
-            .sponsor:not(.inline):not(.big) {
-                margin: 5px;
-                width: 110px;
-                height: 110px;
-            }
-            .sponsor.big {
-                margin: 10px;
-                width: 200px;
-                height: 200px;
-            }
-        }
-
-        @media (max-width: 767px) {
-            .inline {
-                margin: 10px;
-            }
-        }
-
-        @media (max-width: 480px) {
-            .sponsor:not(.inline):not(.big) {
-                margin: 5px;
-                width: 80px;
-                height: 80px;
-            }
-            .sponsor.big {
-                margin: 10px;
-                width: 180px;
-                height: 180px;
-            }
-        }
-
-        @media (min-width: 768px) {
-            .wrapper {
-                margin: 70px 0;
-            }
-            .wrapper--no-margin {
-                margin: 0;
-            }
-            .wrapper--no-padding {
-                padding: 0;
-            }
-            .img-promo {
-                margin: 0;
-            }
-        }
-    </style>
 
 </head>
 <body>
@@ -289,9 +38,9 @@
                         things, noisy things... whatever you decide to create, you’ve got 24 hours to do it!</p>
                     <p>LincolnHack, now in it's second year, is being held to coincide with Lincolnshire Tech Week and moves to a new building. We plan to keep the same great atmosphere, fun and mix of people. From students to veterans all are welcome. If
                         your interested in tech, willing to build something and enjoy meeting like-minded people, we're sure you will enjoy yourself!</p>
-                    <p>LincolnHack is a FREE event. There is no excuse not to come along...</p><a class="btn btn-primary" href="https://www.eventbrite.com/e/lincolnhack-ii-tickets-33165401609">GET TICKETS NOW!</a>
+                    <p>LincolnHack is a FREE event. There is no excuse not to come along...</p><a class="btn lincolhack-flat-button" href="https://www.eventbrite.com/e/lincolnhack-ii-tickets-33165401609">GET TICKETS NOW!</a>
                 </div>
-                <div class="col-sm-5 col-sm-offset-2"><img alt="promo" class="img-responsive img-promo" src="images/promo-1400x1415.png"></div>
+                <div class="col-sm-5 col-sm-offset-2"><img alt="Scenes for the first LincolnHack" class="img-responsive img-promo" src="images/promo-1400x1415.png"></div>
             </div>
         </div>
     </div>
@@ -303,12 +52,12 @@
     <div class="wrapper wrapper--grey wrapper--no-margin">
         <div class="container">
             <div class="row">
-                <div class="col-sm-5"><img alt="promo" class="img-responsive img-promo" src="images/promo-img-1400x1403.png"></div>
+                <div class="col-sm-5"><img alt="Scenes for the first LincolnHack" class="img-responsive img-promo" src="images/promo-img-1400x1403.png"></div>
                 <div class="col-sm-5 col-sm-offset-2">
                     <h3 class="h h--1">When and Where?</h3>
                     <p>LincolnHack is being hosted in the brand new <a href="http://www.lincolnsciencepark.co.uk/boole-technology-centre">Boole Technology Centre</a>, within the grounds of Lincoln Science and Innovation Park.</p>
                     <p>The Boole Technology Centre is named after George Boole, who was born in Lincoln in 1815. This self-taught son of a Boot Maker revolutionised mathematics, not least by defining the algebraic definition of logic. In doing so, he established
-                        the rules of binary code and became the grandfather of the digital age</p><a class="btn btn-primary" href="https://www.eventbrite.com/e/lincolnhack-ii-tickets-33165401609">GET TICKETS NOW!</a>
+                        the rules of binary code and became the grandfather of the digital age</p>
                 </div>
             </div>
         </div>
@@ -503,36 +252,6 @@
                         </div>
                         <div class="panel panel-default">
                             <div class="panel-heading" role="tab">
-                                <h4 class="panel-title"><a class="collapsed" data-parent="#accordion" data-toggle="collapse" href="#c4" role="button">I don't have a team</a></h4>
-                            </div>
-                            <div class="panel-collapse collapse" id="c4" role="tabpanel">
-                                <div class="panel-body">
-                                    We will help you find one on the day.
-                                </div>
-                            </div>
-                        </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading" role="tab">
-                                <h4 class="panel-title"><a class="collapsed" data-parent="#accordion" data-toggle="collapse" href="#c5" role="button">Can I enter more than one challenge?</a></h4>
-                            </div>
-                            <div class="panel-collapse collapse" id="c5" role="tabpanel">
-                                <div class="panel-body">
-                                    You can enter as many as you like… but remember, you only have 24 hours! You can also win more than one challenge.
-                                </div>
-                            </div>
-                        </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading" role="tab">
-                                <h4 class="panel-title"><a class="collapsed" data-parent="#accordion" data-toggle="collapse" href="#c6" role="button">What are the challenges?</a></h4>
-                            </div>
-                            <div class="panel-collapse collapse" id="c6" role="tabpanel">
-                                <div class="panel-body">
-                                    You’ll find out on the day… ;)
-                                </div>
-                            </div>
-                        </div>
-                        <div class="panel panel-default">
-                            <div class="panel-heading" role="tab">
                                 <h4 class="panel-title"><a class="collapsed" data-parent="#accordion" data-toggle="collapse" href="#c7" role="button">What are the prizes?</a></h4>
                             </div>
                             <div class="panel-collapse collapse" id="c7" role="tabpanel">
@@ -563,7 +282,7 @@
                         </div>
                         <div class="panel panel-default">
                             <div class="panel-heading" role="tab">
-                                <h4 class="panel-title"><a class="collapsed" data-parent="#accordion" data-toggle="collapse" href="#c10" role="button">Am I expecte to stay up all night?</a></h4>
+                                <h4 class="panel-title"><a class="collapsed" data-parent="#accordion" data-toggle="collapse" href="#c10" role="button">Am I expected to stay up all night?</a></h4>
                             </div>
                             <div class="panel-collapse collapse" id="c10" role="tabpanel">
                                 <div class="panel-body">
