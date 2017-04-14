678989a0de13cd275644f7cb8f7548a15f8f4834
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-04-14 11:19:54 +0100

    added migrations

---
+++ database/migrations/2017_04_14_101132_messages.php
@@ -0,0 +1,32 @@
+<?php
+
+use Jenssegers\Mongodb\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Messages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siteMessages', function($collection)
+        {
+            $collection->string('message');
+            $collection->string('active');
+            $collection->string('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
