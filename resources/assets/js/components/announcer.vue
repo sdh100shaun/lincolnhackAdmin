adce229ed4ed290f9c1a7fef4a9cc64363fb0441
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-04-16 22:27:27 +0100

    anouncer working with class

---
+++ resources/assets/js/components/announcer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="xs-center" v-bind:class="{ message: hasClass }">
+        <p><strong>{{message}}</strong> </p>
+    </div>
+</template>
+
+<script>
+
+    export default{
+        mounted(){
+
+            window.Echo.channel('message.countdown')
+                    .listen('CountdownMessage', (e) => {
+
+                        this.message = e.message.message;
+                        this.hasClass = true;
+                    });
+
+        },
+        data() {
+            return {
+               message:this.message,
+               hasClass:false
+
+            }
+        }
+    }
+</script>
