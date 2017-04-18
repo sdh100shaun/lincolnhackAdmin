fa524bdbdae5c19fd7a561aaf9d520d343e85a7f
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-04-18 20:47:43 +0100

    tshirt list feature done

---
+++ lincolnhack/Repositories/AttendeeRepository.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shaunhare
+ * Date: 17/04/2017
+ * Time: 12:25
+ */
+
+namespace Lincolnhack\Repositories;
+
+    
+use Illuminate\Contracts\Container\Container;
+use Lincolnhack\Model\Attendee;
+
+class AttendeeRepository
+{
+    /**
+     * @var Container
+     */
+    private $container;
+    
+    /**
+     *
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+    
+    /**
+     *
+     * @return Attendee
+     */
+    private function makeModel():Attendee
+    {
+        $modelName = str_replace('Repositories', 'Model', __CLASS__);
+        $modelName = str_replace('Repository', '', $modelName);
+        
+        return $this->container->make($modelName);
+        
+    }
+    
+    public function getAll()
+    {
+        return $this->makeModel()->all();
+    }
+    
+}
