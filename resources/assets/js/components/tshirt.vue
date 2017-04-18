54dec2fa6e5e70d690ebc0b1fbdddb97d38ee399
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-04-18 20:47:05 +0100

    tshirt list feature done

--- resources/assets/js/components/tshirt.vue
+++ resources/assets/js/components/tshirt.vue
@@ -3,12 +3,28 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">T Shirts</div>
-{{tshirts}}
+
                     <div class="panel-body">
-                        <table  class="table-bordered table" >
-                            <tr v-for="tshirt in tshirts"><td>{{tshirt.size}}</td></tr>
+                        <table  class="table-bordered table-striped table-responsive table" >
+                            <thead><tr>
+                                <th>First name</th>
+                                <th>Last name</th>
+                                <th>Tshirt Size </th>
+                            </tr></thead>
+                            <tbody>
+                            <tr v-for="tshirt in tshirts">
+                                <td>{{tshirt.firstName}}</td>
+                                <td>{{tshirt.lastName}}</td>
+                                <td>{{tshirt.tshirt}}</td>
+                            </tr>
+                            </tbody>
                         </table>
+                        <p>Size Counts:</p>
+                        <ul>
+                           <li v-for="size in sizeData">
+                               {{size.description}} : {{size.number}}
+                           </li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -18,12 +34,20 @@
 
 <script>
 
+    import axios from 'axios';
 
 
     export default {
         mounted() {
             console.log('ready');
-            this.tshirts = this.getTshirtData();
+            this.getTshirtData().then((response)=> {
+                this.tshirts = response.data;
+            })
+            .catch(function (error) {
+                  console.log(error);
+
+            });
+
         },
         props:['source'],
         data(){
@@ -34,11 +58,29 @@
               }
 
         },
-        methods:{
-            getTshirtData:()=>{
-               axios.get('/api/tshirts')
-                return [{size:'xl'}]
+        methods: {
+            getTshirtData: ()=> {
+
+                return axios.get('/api/tshirts')
+
+
             }
+        },
+        computed: {
+
+            sizeData() {
+                var small = this.tshirts.filter((x)=>{return x.tshirt == 'S'})
+                var medium = this.tshirts.filter((x)=>{return x.tshirt == 'M'})
+                var large = this.tshirts.filter((x)=>{return x.tshirt == 'L'})
+                var extralarge = this.tshirts.filter((x)=>{return x.tshirt == 'XL'})
+                var twoXL = this.tshirts.filter((x)=>{return x.tshirt == '2XL'})
+                return [{"description":"small","number":small.length},
+                    {"description":"medium","number":medium.length},
+                    {"description":"large","number":large.length},
+                    {"description":"XL","number":extralarge.length},
+                    {"description":"2XL","number":twoXL.length}];
+            },
+
         }
     }
 </script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+        <h2>Post a message</h2>
+        </div>
+    </div>
     <messages></messages>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+        <h2>Tshirts</h2>
+        </div>
+    </div>
+
     <tshirt source="../api/shirts"></tshirt>
 @endsection
