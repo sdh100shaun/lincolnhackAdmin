8049672f1bcc237cb5c91b981b5ddf5c8145a5da
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-04-28 22:35:27 +0100

    stylign bug css color attendee

--- resources/views/attendee/confirm.blade.php
+++ resources/views/attendee/confirm.blade.php
@@ -7,7 +7,7 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">Lincoln Hack Attendee</div>
 
-                    <div class="panel-body">
+                    <div class="panel-body attendee-confirm">
                         <p> Thanks for signing up. {{$attendee->firstName}}</p>
                         <p>Confirmed : {{$attendee->attendeeId}}</p>
                         <p>T-Shirt: {{$attendee->tshirt}}</p>
@@ -157,26 +157,3 @@
     </script>
 @endsection
 
-
-
-@extends('layouts.form')
-
-@section('content')
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Lincoln Hack Attendee</div>
-
-                    <div class="panel-body">
-                        <p> Thanks for signing up. {{$attendee->firstName}}</p>
-                        <p>Confirmed : {{$attendee->attendeeId}}</p>
-                        <p>T-Shirt: {{$attendee->tshirt}}</p>
-                        <p>Dietary requirements: {{$attendee->dietaryRequirements}}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
