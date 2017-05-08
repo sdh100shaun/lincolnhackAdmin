1d4328b4065dc1bde33b85a778634805ef04d00f
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-05-08 10:23:15 +0100

    added kryptokloud as sponsor

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -197,7 +197,7 @@
                         <a href="https://laser.red" target="_blank"><img alt="Laser Red" src="images/LaserRed600.png"></a>
                     </div>
                     <div class="sponsor big">
-                        <a href="https://http://www.kryptokloud.com/" target="_blank"><img alt="Kryptokloud" src="images/Kryptokloud.png"></a>
+                        <a href="https://http://www.kryptokloud.com/" target="_blank"><img alt="Kryptokloud" src="images/KryptoKloud.png"></a>
                     </div>
                 </div>
                 <h4 class="h h--4">Silver Sponsors</h4>
