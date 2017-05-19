a2f062c942425bd63b9fdbd4376a3369f4cc4583
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-05-19 10:44:07 +0100

    added challeneges to the attendee page

---
+++ resources/assets/js/components/teams.vue
@@ -0,0 +1,42 @@
+
+<script>
+
+    import axios from 'axios';
+
+
+    export default {
+        mounted() {
+            console.log('ready');
+            this.getTeamsData().then((response)=> {
+                this.teams = response.data;
+                console.log(this.teams)
+            })
+                    .catch(function (error) {
+                        console.log(error);
+
+                    });
+
+        },
+        props:['source'],
+        data(){
+            return  {
+
+                token:window.Laravel.csrfToken,
+                teams:[]
+            }
+
+        },
+        methods: {
+            getTeamsData: ()=> {
+
+                return axios.get(this.source)
+
+
+            }
+        },
+        computed: {
+
+
+        }
+    }
+</script>
---
+++ resources/views/partials/challenges.blade.php
@@ -0,0 +1,42 @@
+<div class="container">
+    <div class="row">
+        <h3 class="h h--1 xs-center" id="challenges">Challenges</h3>
+        <p class="xs-center">As a result of the generosity of our sponsors we have a number of challenges for you this weekend.</p>
+
+
+        <div class="row sponsors">
+            <div class="sponsor big">
+                <a href="https://www.epixmedia.co.uk/" target="_blank"><img alt="EpixMedia" src="images/epix-challenge.jpg"></a>
+            </div>
+            <div class="sponsor big">
+                <a href="https://www.khaoscontrol.com/" target="_blank"><img alt="KhaosControl" src="images/khaos-challenge.jpg"></a>
+            </div>
+            <div class="sponsor big">
+                <a href="https://laser.red" target="_blank"><img alt="Laser Red" src="images/lasered-challenge.jpg"></a>
+            </div>
+            <div class="sponsor big">
+                <a href="http://www.kryptokloud.com/" target="_blank"><img alt="Kryptokloud" src="images/krypto-challenge.jpg"></a>
+            </div>
+            <div class="sponsor big">
+                <a href="http://www.blueprintgaming.com/" target="_blank"><img alt="Blueprint gaming" src="images/blueprint-challenge.jpg"></a>
+            </div>
+        </div>
+        <div class="row">
+             <div class="col-sm-8 col-sm-offset-2">
+                <p>Each challenge will be described in full on the day by the sponsor.</p>
+                <p>The fun is in the taking part, but nothing wrong with going home with a prize and there are some great ones
+
+                 </p>
+                 <ul>
+                     <li>Echo dots from Epix Media </li>
+                     <li>HD action Cams from Khaos Control </li>
+                     <li>Steam or Amazon Vouchers from Laser Red </li>
+                     <li>Subscription from Frontend Masters </li>
+                     <li>Jet Brains Product of your choice from Blueprint gaming </li>
+                     <li>Cash Prize of up to £500 from KyrptoKloud </li>
+                 </ul>
+                 <p> So get thinking and be creative ... most of all have fun.  </p>
+            </div>
+        </div>
+    </div>
+</div>
