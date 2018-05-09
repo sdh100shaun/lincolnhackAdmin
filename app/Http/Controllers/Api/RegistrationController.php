ba0f1ee845240fa98f88e031f05d9c53b177d2ad
shaun hare <shaun@shaunhare.co.uk>
2018-05-09 22:20:12 +0100

    updating video - removing large file from cache

---
+++ app/Http/Controllers/Api/RegistrationController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shaunhare
+ * Date: 17/05/2017
+ * Time: 15:47
+ */
+
+namespace app\Http\Controllers\Api;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Lincolnhack\Model\Attendee;
+
+class RegistrationController extends Controller
+{
+    
+    /**
+     * @var Attendee
+     */
+    private $attendee;
+    
+    /**
+     * RegistrationController constructor.
+     * @param Attendee $attendee
+     */
+    public function __construct(Attendee $attendee)
+    {
+        
+        $this->attendee = $attendee;
+    }
+    
+    public function register(Request  $request)
+    {
+        $this->validate($request, [
+            'id' => array('required','Regex:/^[0-9a-fA-F]{24}$/'),
+            'state' => array('required')
+        ]);
+        $id = $request->input("id");
+        $attendee = $this->attendee->find($id);
+        $state = $request->input('state');
+        
+        $attendee->registered = (bool) $state;
+        $attendee->save();
+        return response()->json(array("registered"=>$state));
+    }
+}
