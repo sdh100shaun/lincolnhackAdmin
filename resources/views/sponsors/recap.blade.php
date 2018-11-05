e55601a575044519eb5fca99c53936f94a0b896b
Shaun Hare <shaun.hare@dvsa.gov.uk>
2018-11-05 21:49:21 +0000

    updated sponsor info

--- resources/views/sponsors/recap.blade.php
+++ resources/views/sponsors/recap.blade.php
@@ -11,9 +11,9 @@
         <div class="row">
             <div class="col-sm-12">
                 <div class="center-block sponsor-page">
-                    <img width="95%" src="../../images/assets/sponsors/recap-io.svg" alt="Recap IO" />
+                    <img width="95%" src="../../images/assets/sponsors/recap-io.svg" alt="Recap IO"/>
                 </div>
-                <hr />
+                <hr/>
             </div>
         </div>
     </div>
@@ -23,17 +23,30 @@
             <div class="col-sm-6">
 
                 <p>The safest way to keep track of your crypto finances.</p>
-                   <p> Founded in 2018, recap is a privacy focused digital asset portfolio and tax management platform, designed for both businesses and consumers.
+                <p> Founded in 2018, recap is a privacy focused digital asset portfolio and tax management platform,
+                    designed for both businesses and consumers.
 
                 </p>
-            <p>Recap securely connects to exchanges to read your trades and monitors crypto blockchains to identify your transactions.
+                <p>Recap securely connects to exchanges to read your trades and monitors crypto blockchains to identify
+                    your transactions.
 
-                We’re privacy focussed so we’ll never see your data unless you opt-in to advanced features.</p></div>
-        <div class="col-sm-5 col-sm-offset-1">
+                    We’re privacy focussed so we’ll never see your data unless you opt-in to advanced features.</p>
+            </div>
+            <div class="col-sm-5 col-sm-offset-1">
                 <h5 class="recap"> The Recap Challenge Challenge </h5>
 
-                <p>The Recap io challenge this year will be announced soon.</p>
-
+                <p>As 2018 marks the 10th anniversary of Bitcoin, to celebrate, Recap would like you to:
+                </p>
+                <blockquote><strong>Come up with a novel use for Crypto.</strong>
+                    We're looking for something with solid network effects, a couple ideas for you are:
+                    <ul>
+                        <li>Something to get your Gran using crypto - meals on wheels paid in cryptocurrency?
+                        </li>
+                        <li>A crypto swear box? Drop the # bomb and make your colleagues pay up!</li>
+                    </ul>
+                </blockquote>
+               <p> The team prize for the best project will be 0.04 bitcoin (4 members per team === 0.01BTC per team member)
+                   </p>
             </div>
         </div>
         <div class="row">
@@ -58,7 +71,10 @@
                 <div class="row">
                     <div class="col-md-9 col-md-offset-3 col-sm-11 col-sm-offset-1 col-xs-12 col-xs-offset-0">
                         <div class="speech-bubble">
-                            <p class="text-center" id="answer">Challenge prizes yet to be announced but expect good things</p>
+                                <p class="text-center" id="answer"><img src="../../images/Bitcoin.png" alt="Bitcoin" />
+                                    A cool 0.04 BTC* loaded onto paper wallet (£200 for you old
+                                    schoolers).
+                                </p>
                         </div>
                     </div>
                 </div>
